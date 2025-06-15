52de64fc99cea995ccf8cfb1d54849b77b8f1205
Piotr <hunter976@gmail.com>
2025-06-16 00:56:01 +0200

    trying to make imgs work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { sendJSON, filterData, getRandomCar } from './utilities.js';
 import { cars } from './data.js';
+import { createReadStream, existsSync } from 'node:fs';
 
 const PORT = process.env.PORT || 8000;
 
@@ -38,6 +39,22 @@ const server = http.createServer((req, res) => {
     res.end(sendJSON(res, 200, randomCar));
     return;
   }
+
+  if (req.url.startsWith('/images/')) {
+    const imageName = req.url.split('/').pop();
+    const imagePath = `./images/${imageName}`;
+
+    if (existsSync(imagePath)) {
+      const stream = createReadStream(imagePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      stream.pipe(res);
+    } else {
+      res.end('not found');
+    }
+
+    return;
+  }
+
   res.end(
     sendJSON(
       res,
